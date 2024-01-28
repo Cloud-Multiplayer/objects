515e8f04e2e403ff3009b531693592207cd11a1b
Elijah De Guzman <elijahdeguzman@Elijahs-MBP.attlocal.net>
2024-01-28 15:09:18 -0500

    Added text file to repo

---
+++ java.js
@@ -0,0 +1,6 @@
+var firstName = "Elijah";
+var lastName = "De Guzman";
+var age = 17;
+alert(firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("I BE HAVING THE MOST FUN IN THIS CLASS, YEAHHHHHH");
