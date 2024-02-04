e8dc2100a159b8803d0cf88825a89c78435e11f2
Elijah De Guzman <elijahdeguzman@Elijahs-MBP.attlocal.net>
2024-02-03 19:58:59 -0500

    conditional assignment file'

--- java.js
+++ java.js
@@ -1,6 +1,32 @@
-var firstName = "Elijah";
-var lastName = "De Guzman";
-var age = 17;
-alert(firstName + " " + lastName);
-alert("I am " + age + " years old");
-console.log("I BE HAVING THE MOST FUN IN THIS CLASS, YEAHHHHHH");
+var userChoice, randomNumber, computerChoice;
+userChoice = prompt("heads or tails");
+randomNumber = Math.round(Math.random());
+if(randomNumber == 0)
+{
+    computerChoice = "heads";
+}
+else{
+    computerChoice = "tails";
+}
+if(computerChoice == userChoice)
+{
+    alert("You guessed right! The coin flip landed on " + computerChoice);
+}
+else
+{
+    alert("Sorry, the coin flip landed on " + computerChoice);
+}
+
+
+var birthYear;
+birthYear = prompt("What year were you born?");
+var currentYear = 2024;
+var age = currentYear - birthYear;
+
+if (age > 21) {
+    alert("You are old enough to drink in the US");
+} else if (age === 21) {
+    alert("You are old enough to drink in the US...barely");
+} else {
+    alert("Sorry, you are not old enough to drink in the US");
+}
