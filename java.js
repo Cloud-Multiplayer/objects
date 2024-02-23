03635636e432c708c4ad841426a54162bb0268b7
Elijah De Guzman <elijahdeguzman@Elijahs-MBP.attlocal.net>
2024-02-22 20:48:40 -0500

    project file

--- java.js
+++ java.js
@@ -1,17 +1,45 @@
-for (let i = 1000; i >= 0; i -= 2) {
-    console.log(i);
-}
-for (let i = 0; i <= 10000; i++) {
-    if (i === 2500) {
-        alert("A quarter of the way there!");
-    } else if (i === 5000) {
-        alert("Halfway there!");
-    } else if (i === 10000) {
-        alert("The loop is done!");
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+
+var rounds = parseInt(prompt("How many rounds do you want to play?"));
+
+for (let i = 0; i < rounds; i++) {
+    var userChoice = prompt("Enter r, p, or s (for rock, paper, or scissors)");
+
+    var computerChoice = Math.floor(Math.random() * 3);
+    var computerMove;
+    if(computerChoice === 0)
+    {
+        computerMove = "r";
+    }
+    else if (computerChoice === 1)
+    {
+        computerMove = "p";
+    }
+    else
+    {
+        computerMove = "s";
+    }
+
+    if (userChoice === computerMove) 
+    {
+        ties++;
+        alert("It's a tie!");
+    } else if (
+        (userChoice === "r" && computerMove === "s") ||
+        (userChoice === "p" && computerMove === "r") ||
+        (userChoice === "s" && computerMove === "p")
+    ) 
+    {
+        wins++;
+        alert("You won!");
+    } else 
+    {
+        losses++;
+        alert("You lost!");
     }
 }
-var favoriteShows = ["The Mandalorian", "WandaVision", "Daredevil", "Masters of the Air", "For All Mankind"];
 
-for (let i = 0; i < favoriteShows.length; i++) {
-    console.log("My #" + (i+1) + " favorite TV show is " + favoriteShows[i]);
-}
+alert("Game Over! You won " + wins + " times, tied " + ties + " times, and lost " + losses + " times.");
