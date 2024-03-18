4307347ec59310fd18c7c4c8327cfbc7e16d26d9
Elijah De Guzman <elijahdeguzman@Elijahs-MBP.attlocal.net>
2024-03-17 23:06:18 -0400

    object files

--- java.js
+++ java.js
@@ -1,20 +1,34 @@
+let movies = [
+  {
+    name: "Interstellar",
+    year: "2014",
+    director: "Christopher Nolan",
+    picture: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpghttps://i.ytimg.com/vi/YF1eYbfbH5k/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGE4gYShlMA8=&rs=AOn4CLDt6JPoesvmQnP8qf-00JpeDZUfyA"
+  },
+  {
+    name: "Everything Everywhere All At Once",
+    year: "2022",
+    director: "Daniel Kwan and Daniel Scheinert",
+    picture: "https://i.etsystatic.com/18242346/r/il/7fb6f1/3898744406/il_fullxfull.3898744406_6wp7.jpg"
+  },
+  {
+    name: "Jojo Rabbit",
+    year: "2019",
+    director: "Taika Waititi",
+    picture: "https://m.media-amazon.com/images/M/MV5BZjU0Yzk2MzEtMjAzYy00MzY0LTg2YmItM2RkNzdkY2ZhN2JkXkEyXkFqcGdeQXVyNDg4NjY5OTQ@._V1_.jpg"
+  }
+];
 
-let main = document.querySelector('main');
+let movieElements = movies.map(movie => {
+  return `
+    <li>
+      <p>${movie.name}</p>
+      <p>Year: ${movie.year}</p>
+      <p>Director: ${movie.director}</p>
+      <img src="${movie.picture}" alt="${movie.name} Poster"  
+      style="max-width: 50%;">
+    </li>
+  `;
+});
 
-let joke1 = 'A guy walks into a bar and asks for 1.4 root beers. The bartender says “I’ll have to charge you extra, that’s a root beer float”. The guy says “In that case, better make it a double.';
-let joke2 = "Knock, knock. Who’s There? Very long pause… “Java.”";
-let joke3 = "Programming is 10% writing code and 90% understanding why it’s not working";
-
-let template = `
-  <h1>My Jokes</h1>
-  <ul>
-    <li>${joke1}</li>
-    <li>${joke2}</li>
-    <li>${joke3}</li>
-  </ul>
-`;
-
-main.innerHTML = template;
-let para = document.createElement('p');
-para.textContent = "That's all folks!";
-document.body.appendChild(para);
+document.getElementById('movie-list').innerHTML = movieElements.join('');
